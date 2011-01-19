230665bc0a29d307c77818846dc5c9804419ac52
Ethan Chen <intervigil@gmail.com>
2011-01-18 23:14:22 -0800

    don't need this old linker flag anymore

--- jni/autotalent/Android.mk
+++ jni/autotalent/Android.mk
@@ -4,6 +4,6 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := autotalent
 LOCAL_SRC_FILES := mayer_fft.c fft.c autotalent.c autotalent-interface.c
-LOCAL_LDLIBS := -L$(SYSROOT)/usr/lib -llog
+LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)

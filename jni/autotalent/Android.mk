38476cd57c858f13b96da4e06d98335a36bcb8d5
Ethan Chen <intervigil@gmail.com>
2011-01-18 23:44:54 -0800

    add includes

--- jni/autotalent/Android.mk
+++ jni/autotalent/Android.mk
@@ -4,6 +4,7 @@ include $(CLEAR_VARS)
 
 LOCAL_MODULE := autotalent
 LOCAL_SRC_FILES := mayer_fft.c fft.c autotalent.c autotalent-interface.c
+LOCAL_C_INCLUDES := mayer_fft.h fft.h autotalent.h autotalent-interface.h
 LOCAL_LDLIBS := -llog
 
 include $(BUILD_SHARED_LIBRARY)

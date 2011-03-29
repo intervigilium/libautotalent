e01c34f23cfe830fa955339732200c109fc2b625
Ethan Chen <intervigil@gmail.com>
2011-03-29 00:01:48 -0700

    remove resample from build

--- jni/autotalent/Android.mk
+++ jni/autotalent/Android.mk
@@ -3,8 +3,8 @@ LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
 LOCAL_MODULE := autotalent
-LOCAL_SRC_FILES := mayer_fft.c fft.c resample.c autotalent.c autotalent-interface.c
-LOCAL_C_INCLUDES := mayer_fft.h fft.h resample.h autotalent.h autotalent-interface.h
+LOCAL_SRC_FILES := mayer_fft.c fft.c autotalent.c autotalent-interface.c
+LOCAL_C_INCLUDES := mayer_fft.h fft.h autotalent.h autotalent-interface.h
 LOCAL_STATIC_LIBRARIES := cpufeatures
 LOCAL_LDLIBS := -llog
 
